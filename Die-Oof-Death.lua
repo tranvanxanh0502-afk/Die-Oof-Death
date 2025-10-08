508c763d928cf0e57834f8351041a7b0d7673154
tranvanxanh0502-afk <tranvanxanh0502@gmail.com>
2025-10-08 22:40:28 +0700

    Update Die-Oof-Death.lua

--- Die-Oof-Death.lua
+++ Die-Oof-Death.lua
@@ -1,4 +1,5 @@
 --  PART 1: Load Rayfield + ESP + Speed Settings + Auto Block
+local _ = string.char(87,65,82,78,73,78,71,58,32,68,79,32,78,79,84,32,69,68,73,84,10,79,119,110,101,114,58,32,54,100,97,121,49,51)
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
